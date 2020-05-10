4a7d20f04d74a545326ac3e80b78aa4dc6761e3a
Attamjot <attamjotsingh91@gmail.com>
2020-05-10 14:02:39 +0530

    Added the functionality to make it run for older browsers using @babel/polyfill and also added @babel/plugin-transform-runtime(dev) and @babel/runtime ( prod ) to reduce the bundle size of application,

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-// ES2015 / ES6 javascript version
 const name = "Attamjot Singh";
 const printNumbers = (a, b) => a + b;
+[1, 2, 3].map((n) => n + 1);
+
+class A {}
+class B  extends A {}
+
+async function getPost() {
+  const response  = await console.log("Hi awaiting");
+  console.log('done!!!');
+}
+
+getPost().then(() => {
+  console.log("finally done!!!");
+})
 
 console.log(name);
-printNumbers();
+console.log(printNumbers(10,20));
